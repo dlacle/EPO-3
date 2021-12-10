eda559fbaf99ef8ef7273ed1d13f679cd6eca1af
dlacle <85848688+dlacle@users.noreply.github.com>
2021-12-10 09:41:44 -0800

    !

--- mem test/erase_mem.vhd
+++ mem test/erase_mem.vhd
@@ -16,14 +16,13 @@ end entity erase_test;
 
 architecture behavioural of erase_test is
 
-    type state_type is (idle_state, opcode_state,address_state, w_state);
+    type state_type is (idle_state,address_opcode,opcode_wel_e,idle_wel_e,opcode_erase,idle_erase);
 
     signal state, new_state       : state_type;
     signal clkcount, new_clkcount : integer range 0 to 800;
     signal bitcount, new_bitcount : integer range 0 to 2;
-    signal opcode : std_logic_vector(7 downto 0) := "11011000";
+    signal opcode,new_opcode : std_logic_vector(7 downto 0) := "00000000";
     signal address, new_address : std_logic_vector(23 downto 0) := "000000000000000000000000";
-    signal frame_count, new_frame_count: integer range 0 to 70;
 
 
 begin
@@ -34,72 +33,100 @@ reg: process (clk25)
             if reset = '1' then
                 state    <= idle_state;
                 clkcount <= 0;
-		frame_count<= 0;
+		opcode	<= (others => '0');
             else
                 state    <= new_state;
                 clkcount <= new_clkcount;
-		frame_count <=new_frame_count;
+		opcode <= new_opcode;
             end if;
         end if;
     end process;
 
-comb: process (state, clkcount, clk25, miso, opcode,frame_count,start_erase)
+comb: process (state, clkcount, clk25, miso, opcode,start_erase)
     begin
         case state is
         when idle_state =>
             cs      <= '1';
             mosi    <= '0';
             sck<='0';
+        new_opcode <= "00000110";
 		new_clkcount<=0;
-        new_frame_count<=0;
 
             if start_erase = '1' then
-                new_state <= opcode_state;
+                new_state <= opcode_wel_e;
             else
                 new_state <= idle_state;
             end if;
 
-        when opcode_state =>
-        new_frame_count<=frame_count;
+    when opcode_wel_e =>
+      cs <=  '0';
+      sck <= not(clk25);
+      mosi <= opcode(7-clkcount);
+      new_opcode <= opcode;
+      if (clkcount = 7) then
+        new_state <= idle_wel_e;
+        new_clkcount <= 0;
+      else
+        new_state <= opcode_wel_e;
+        new_clkcount <= clkcount + 1;
+      end if;
+    
+    when idle_wel_e =>
+      cs <=  '1';
+      sck <= '0';
+      mosi <= '0';
+      if (clkcount = 10) then
+        new_opcode <= "11011000";
+        new_state <= opcode_erase;
+        new_clkcount <= 0;
+      else
+        new_opcode <= "00000000";
+        new_state <= idle_wel_e;
+        new_clkcount <= clkcount + 1;
+      end if;
+
+
+
+        when opcode_erase =>
               cs      <= '0';
          sck     <= not(clk25);
         mosi    <= opcode(7-clkcount);
+ 	new_opcode <= opcode;
   
           new_clkcount <= clkcount + 1;
          if clkcount = 7 then
-            new_state <= address_state;
+            new_state <= address_opcode;
             new_clkcount <= 0;
             else
-            new_state <= opcode_state;
+            new_state <= opcode_erase;
         end if;
 
-        when address_state =>
-        new_frame_count<=frame_count;
+        when address_opcode =>
         cs      <= '0';
         sck     <= not(clk25);
         mosi    <= address(23-clkcount);
         new_clkcount <= clkcount + 1;
         if clkcount = 23 then
-        new_state <= w_state;
+        new_state <= idle_erase;
         new_clkcount <= 0;
         else
-        new_state <= address_state;
+        new_state <= address_opcode;
         end if;
 
-        when w_state =>
+        when idle_erase =>--
 	cs      <= '0';
         sck     <= not(clk25);
 
         if start_erase = '1' then
-        new_frame_count<=frame_count+1;
+        new_clkcount <= clkcount + 1;
         else 
-        new_frame_count<=frame_count;
+        new_clkcount <= clkcount;
         end if;
 
-        if frame_count>=60 then
+        if clkcount >= 60 then
         new_state<= idle_state;
         else
-        new_state<=w_state;
+        new_state<=idle_erase;
         end if;
 
        end case;
