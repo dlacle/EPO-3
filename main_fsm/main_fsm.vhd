13fba4376904b70d15c361eb056f1c43d4adf3cd
dlacle <85848688+dlacle@users.noreply.github.com>
2021-12-14 09:20:37 +0100

    update_main

--- main_fsm/main_fsm.vhd
+++ main_fsm/main_fsm.vhd
@@ -12,7 +12,8 @@ entity main_fsm is
             start_read      : in std_logic;
             start_startup   : out std_logic; 
             start_erase     : out std_logic;
-            frame_full      : in std_logic;   
+            frame_full      : in std_logic;
+            mode            : out std_logic_vector ( 1 downto 0);    
             frame_begin     : out std_logic
     );
 end main_fsm;
@@ -23,7 +24,6 @@ architecture behavioural of main_fsm is
 
 type state_type is (idle,idle_startup, opcode_wel, idle_wel, opcode_unprotect, data_unprotect, idle_unprotect);   
  signal count: integer:=1;
- signal tmp : std_logic := '0';
 
 begin
 
@@ -49,101 +49,26 @@ begin
     combinatorial: process (state, clk25, clkcount, opcode, debug)
     begin
       case state is
-        when idle_startup =>
-            cs              <= '1';
-            sck             <= '0';
-            mosi            <= '0';
-            new_debug       <= "10000000";
-            new_clkcount    <= 0;
-        if (clkcount = 10) then
-            new_opcode      <= "00000110";
+        when startup_state =>
+            mode = "000";
+        if (startup_done = '1') then
             new_state       <= opcode_wel;
         else
-            new_opcode      <= "00000000";
-            new_state       <= idle_startup;
-            new_clkcount    <= clkcount + 1;
+            new_state       <= startup;
         end if;
         
         
-        when opcode_wel =>
-            cs              <=  '0';
-            sck             <= not(clk25);
-            mosi            <= opcode(7-clkcount);
-            new_debug       <= "10000001";
-            new_opcode      <= opcode;
-        
-        if (clkcount = 7) then
-            new_state       <= idle_wel;
-            new_clkcount    <= 0;
+        when chip_erase_state =>
+            mode = "001";
+        if chip_erase_done = '1' then
+            new_state       <= write_state;
         else
-            new_state       <= opcode_wel;
-            new_clkcount    <= clkcount + 1;
+            new_state       <= chip_erase_state;
         end if;
         
-        when idle_wel =>
-            cs              <= '1';
-            sck             <= '0';
-            mosi            <= '0';
-            new_debug       <= "10000010";
-        
-        if (clkcount = 10) then
-            new_opcode      <= "00000001";
-            new_state       <= opcode_unprotect;
-            new_clkcount    <= 0;
-        else
-            new_opcode      <= "00000000";
-            new_state       <= idle_wel;
-            new_clkcount    <= clkcount + 1;
-        end if;
-        
-        when opcode_unprotect =>
-            cs              <=  '0';
-            sck             <= not(clk25);
-            mosi            <= opcode(7-clkcount);
-            new_debug       <= "10000100";
-            new_opcode      <= opcode;
-        
-        if (clkcount = 7) then
-            new_state       <= data_unprotect;
-            new_clkcount    <= 0;
-        else
-            new_state       <= opcode_unprotect;
-            new_clkcount    <= clkcount + 1;
-        end if;
-        
-        when data_unprotect =>
-            cs              <= '0';
-            sck             <= not(clk25);
-            mosi            <= '0';
-            new_debug       <= "10001000";
-            new_opcode      <= "00000000";
-        
-        if (clkcount = 7) then
-            new_state       <= idle_unprotect;
-            new_clkcount    <= 0;
-        else
-            new_state       <= data_unprotect;
-            new_clkcount    <= clkcount + 1;
-        end if;
-        
-        when idle_unprotect =>
-            cs              <= '1';
-            sck             <= '0';
-            mosi            <= '0';
-            new_debug       <= "10010000";
-        
-        if (clkcount = 10) then
-            new_opcode      <= "00000110";
-            new_state       <= idle;
-            new_clkcount    <= 0;
-        else
-            new_opcode      <= "00000000";
-            new_state       <= idle_unprotect;
-            new_clkcount    <= clkcount + 1;
-        end if;
-
-        when read_state =>
-        
+        when write_state =>
+            mode = "010";
+        if dead_time = '1' then
 
       end case;
     end process;
