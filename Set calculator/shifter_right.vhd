a2dded9c09e4ec2e72231bae85987d9cb8043678
Emmad <kostenukmikahil@gmail.com>
2022-01-07 14:02:37 +0100

    Collab
    Collab

--- Set calculator/shifter_right.vhd
+++
@@ -1,9 +0,0 @@
-library IEEE;
-use IEEE.std_logic_1164.ALL;
-
-entity shifter_right is
-   port(s_in : in  std_logic_vector(11 downto 0);
-	 s_out : out std_logic_vector(11 downto 0)
-);
-end shifter_right;
-
