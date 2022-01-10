f7451378908e9193ad46d34ad51b813a2dad5ffe
marickvermeule <84080053+marickvermeule@users.noreply.github.com>
2022-01-10 18:46:36 +0100

    Add files via upload

--- Position & zoom/buffer.vhd
+++ Position & zoom/buffer.vhd
@@ -5,6 +5,7 @@ use IEEE.numeric_std.all;
 entity register_a is
 	port (	clk		: in 	std_logic;
 		enable		: in 	std_logic;
+		reset		: in std_logic;
 		a_in 		: in	std_logic_vector(11 downto 0);
 		a_out	 	: out 	std_logic_vector(11 downto 0)
 	);
@@ -13,21 +14,30 @@ end entity register_a;
 architecture structural of register_a is 
 
 type	register_state is (						update_state,
-							wait_state
+							wait_state,
+							reset_state
 							);	
 
 signal state, new_state	: register_state;
 begin 
-	process (clk)
+process (clk)
 	begin
 		if (rising_edge (clk)) then
+			if (reset = '1') then
+				state <= reset_state;	-- Reset arrow
+			else
 				state	<= new_state;	-- going to the new state
+			end if;
 		end if;
 	end process;
 
-	process(a_in, clk, enable)
+
+	process(enable,state,a_in)
 	begin
 		case state is 
+			when reset_state =>
+				new_state <= wait_state;
+				a_out <= "000000000000";
 			when update_state =>
 				a_out <= a_in;
 				new_state <= wait_state;
